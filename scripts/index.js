8a73561019d7f8cea0ea67a49d9647d73fdba12d
mkdir-helen <experiment.helen@gmail.com>
2018-10-11 09:36:37 -0400

    adds comments and data attribute

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+
+
+//1.Make some code run when 
+//I click the button
+const triggerElement = document.querySelector();
+
+
+//2. Write some text to the page 
+//on button cick
+
+//3. Pull text from an array of text 
+//to write to the page.
