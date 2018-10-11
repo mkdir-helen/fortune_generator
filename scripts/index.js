60a81ec200a5afb5f93eea781271cd7ee1ad51a3
mkdir-helen <experiment.helen@gmail.com>
2018-10-11 10:32:39 -0400

    adds addEventListener

--- scripts/index.js
+++ scripts/index.js
@@ -2,11 +2,16 @@
 
 //1.Make some code run when 
 //I click the button
-const triggerElement = document.querySelector();
+const triggerElement = document.querySelector('[data-trigger]');
 
+function hello(){
+    console.log('Hello addEventListener!');
+}
 
+triggerElement.addEventListener('click', hello);
 //2. Write some text to the page 
 //on button cick
+const outputElement = document.querySelector('[data-output]');
 
 //3. Pull text from an array of text 
 //to write to the page.
